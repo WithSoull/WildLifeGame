451bbaea131af940b248d2cdac81f64111f58eb4
WithSoull <withsoull@yandex.ru>
2023-11-05 00:43:37 +0300

    v2.0.0 fully rewrite on NCURSES

---
+++ parse.cpp
@@ -0,0 +1,13 @@
+#include "parse.h"
+
+float get_max_count_herb(){
+    return 0.0;
+}
+
+float get_max_count_pred(){
+    return 0.0;
+}
+
+float get_max_count_grass(){
+    return 0.0;
+}
---
+++ parse.h
@@ -0,0 +1,6 @@
+#pragma once
+
+
+float get_max_count_herb();
+float get_max_count_pred();
+float get_max_count_grass();
